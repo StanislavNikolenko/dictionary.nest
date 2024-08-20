19dd17db2adfb7fa073bdbaf82a854f2bb8bd0fe
Stanislav Nikolenko <stanislavnikolenko@stanislavnikolenko.lan>
2024-08-21 00:12:04 +0400

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle("Dictionary app")
     .setDescription("The API of the dictionary application")
