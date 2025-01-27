08537d026896f68daff1b78e9d1bd3d58e6ab817
Stanislav <stan.nikolenko89@gmail.com>
2025-01-27 15:16:45 +0300

    chore: test push 2

--- src/concept/concept.service.ts
+++ src/concept/concept.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { NotFoundException } from "@nestjs/common";
